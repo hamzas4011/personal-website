90001ddafea3551eeffd5bc836a1b0378be9cbc2
Hamza Simsek <hamzas4011@hotmail.com>
2025-02-10 11:42:15 +0100

    n

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,14 +1,20 @@
 import React from "react";
-import "../styles/Intro.css"; 
+import "../styles/Intro.css";
+import profilePicture from "../assets/profile-picture.jpg"; 
 
 export function Intro() {
   return (
     <section id="intro" className="intro">
       <div className="intro-content">
+        {/* Profile Picture */}
+        <img src={profilePicture} alt="Hamza Simsek" className="profile-pic" />
+
+        {/* Name & Role */}
         <h1>Hi, I'm Hamza Simsek 👋</h1>
         <h2>👨‍💻 Full-Stack Developer | Accessibility | Universal Design</h2>
         <p>Building inclusive and user-friendly web applications.</p>
 
+        {/* Call-to-Action Buttons */}
         <div className="intro-buttons">
           <a href="#about" className="button primary-btn">About Me</a>
           <a href="#projects" className="button secondary-btn">My Projects</a>
