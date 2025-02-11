923e81a2d8cb2c559bfe8a92d32dd1d2fb56e5b8
Hamza Simsek <hamzas4011@hotmail.com>
2025-02-11 20:58:44 +0100

    final update about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,17 +1,37 @@
 import React from "react";
 import "../styles/About.css"; 
+import { FaLaptopCode, FaGamepad, FaHiking, FaBook } from "react-icons/fa"; 
 
 export function About() {
   return (
     <section id="about" className="about">
-      <div className="about-content">
+      <div className="about-box">
         <h2>About</h2>
         <p>
           I'm a Full-Stack Developer with a Master's in Applied Computer and IT, specializing in Accessibility & Universal Design. 
-          I love building applications that make technology more inclusive and user-friendly. 
-          Currently, I'm expanding my backend skills with Node.js to become a more complete developer. 
+          I love building applications that make technology more inclusive and user-friendly.
+        </p>
+        <p>
           Outside of coding, I enjoy reading, playing video games, and hiking in nature to recharge.
         </p>
+        <div className="about-icons">
+          <div className="about-item">
+            <FaLaptopCode className="icon" />
+            <span>Programming</span>
+          </div>
+          <div className="about-item">
+            <FaBook className="icon" />
+            <span>Reading</span>
+          </div>
+          <div className="about-item">
+            <FaGamepad className="icon" />
+            <span>Gaming</span>
+          </div>
+          <div className="about-item">
+            <FaHiking className="icon" />
+            <span>Hiking</span>
+          </div>
+        </div>
       </div>
     </section>
   );
