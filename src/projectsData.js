5981cd3030bd91358f9a420d7f1f10c50b1fce5f
Hamza Simsek <hamzas4011@hotmail.com>
2025-02-12 11:59:48 +0100

    fixed the github links in projectsData

--- src/projectsData.js
+++ src/projectsData.js
@@ -2,26 +2,26 @@
 
 const projects = [
     {
-      name: "Project 1",
-      description: "A cool web app built with React and Node.js.",
-      liveLink: "https://coffee-shop-ivory-seven.vercel.app",
-      githubLink: "https://github.com/hamzas4011/coffee-shop",
-    },
-    {
-      name: "Project 2",
-      description: "An e-commerce site made with Next.js and Firebase.",
+      name: "Calculator App",
+      description: "Responsive calculator built with React.js for basic arithmetic operations.",
       liveLink: "https://calculator-app-two-beta.vercel.app/",
       githubLink: "https://github.com/hamzas4011/calculator-app",
     },
     {
+      name: "Coffee Shop",
+      description: "Coffee shop website built with React.js, creatively showcasing menu and services.",
+      liveLink: "https://coffee-shop-ivory-seven.vercel.app/",
+      githubLink: "https://github.com/hamzas4011/coffee-shop",
+    },
+    {
       name: "Typing Tester",
-      description: "A Python-based CLI tool for data analysis.",
+      description: "Python-based typing tester with a simple, interactive interface for practice and tracking progress.",
       liveLink: null,
       githubLink: "https://github.com/hamzas4011/Typing_Tester",
     },
     {
       name: "Konya Restaurant",
-      description: "A simple to-do app using React and LocalStorage.",
+      description: "Vue.js frontend for a fictional Turkish restaurant, linked to a C# API with CRUD.",
       liveLink: null,
       githubLink: "https://github.com/hamzas4011/konyarestaurant",
     }
