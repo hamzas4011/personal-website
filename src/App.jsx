2209d994de2fb9fca819e123c3fdb052748c59f2
Hamza Simsek <hamzas4011@hotmail.com>
2025-02-12 16:39:25 +0100

    deleted app.css

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import "./App.css";
 
 export default function App() {
   return (
