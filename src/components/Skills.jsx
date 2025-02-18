556eaac746cbdff4ef29e47a78ca481f022d56aa
Hamza Simsek <hamzas4011@hotmail.com>
2025-02-18 20:09:24 +0100

    create skills.jsx

---
+++ src/components/Skills.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "../styles/Skills.css";
+
+export function Skills() {
+  return (
+    <section id="skills" className="skills">
+      <h2>Technical Skills</h2>
+      <div className="skills-list">
+        <span>React.js</span>
+        <span>JavaScript</span>
+        <span>HTML & CSS</span>
+        <span>Node.js</span>
+        <span>Git & GitHub</span>
+        <span>Accessibility (WCAG)</span>
+        <span>Python (Basic)</span>
+      </div>
+    </section>
+  );
+}
+
+export default Skills;
