434e7b1b8d965ec0ae106acca0a35f8cb5eadb25
Hamza Simsek <hamzas4011@hotmail.com>
2025-02-18 20:14:23 +0100

    skills section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Intro from "./components/Intro";
 import About from "./components/About";
+import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
@@ -10,6 +11,7 @@ function App() {
     <>
       <Intro />
       <About />
+      <Skills />
       <Projects />
       <Contact />
       <Footer />
