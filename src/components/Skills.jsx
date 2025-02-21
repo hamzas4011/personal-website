fb789d20568e147b647bdea227168b8fa147b277
Hamza Simsek <hamzas4011@hotmail.com>
2025-02-21 23:12:22 +0100

    error code

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/Skills.css";
-import { FaJs, FaPython, FaHtml5, FaCss3, FaReact, FaNodeJs, FaGitAlt, FaAws, FaDatabase, FaCsharp, FaCode } from "react-icons/fa";
+import { FaJs, FaPython, FaHtml5, FaCss3, FaReact, FaNodeJs, FaGitAlt, FaGithub } from "react-icons/fa";
 
 const Skills = () => {
   return (
@@ -11,32 +11,26 @@ const Skills = () => {
         <div className="skills-category">
           <h3>Programming Languages</h3>
           <div className="skills-list">
-            <FaJs className="skill-icon js" />
-            <FaHtml5 className="skill-icon html" />
-            <FaCss3 className="skill-icon css" />
-            <FaPython className="skill-icon python" />
-            <FaCsharp className="skill-icon csharp" />
+            <div className="skill-item"><FaJs className="skill-icon js" /> <span>JavaScript</span></div>
+            <div className="skill-item"><FaPython className="skill-icon python" /> <span>Python</span></div>
+            <div className="skill-item"><FaHtml5 className="skill-icon html" /> <span>HTML</span></div>
+            <div className="skill-item"><FaCss3 className="skill-icon css" /> <span>CSS</span></div>
           </div>
         </div>
 
         <div className="skills-category">
           <h3>Frameworks & Libraries</h3>
           <div className="skills-list">
-            <FaReact className="skill-icon react" />
-            <FaNodeJs className="skill-icon nodejs" />
-            <FaCode className="skill-icon pygame" />
-            <FaCode className="skill-icon matplotlib" />
-            <FaCode className="skill-icon tkinter" />
-            <FaCode className="skill-icon dotnet" />
+            <div className="skill-item"><FaReact className="skill-icon react" /> <span>React.js</span></div>
+            <div className="skill-item"><FaNodeJs className="skill-icon nodejs" /> <span>Node.js</span></div>
           </div>
         </div>
 
         <div className="skills-category">
           <h3>Development Tools</h3>
           <div className="skills-list">
-            <FaGitAlt className="skill-icon git" />
-            <FaAws className="skill-icon aws" />
-            <FaDatabase className="skill-icon sql" />
+            <div className="skill-item"><FaGitAlt className="skill-icon git" /> <span>Git</span></div>
+            <div className="skill-item"><FaGithub className="skill-icon github" /> <span>GitHub</span></div>
           </div>
         </div>
       </div>
