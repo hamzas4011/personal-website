041cd496aa665a6bc11c5878e5d25c6c5ba14267
Hamza Simsek <hamzas4011@hotmail.com>
2025-02-21 23:48:11 +0100

    update contact page, colors

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,18 +7,18 @@ export function Contact() {
     <section id="contact" className="contact">
       <div className="contact-box">
         <h2>Contact</h2>
-        <p>Feel free to contact via email or LinkedIn, or explore my projects on GitHub.</p>
+        <p>Feel free to contact me via email or LinkedIn, or explore my projects on GitHub.</p>
 
         <div className="contact-links">
-          <a href="mailto:hamzas4011@hotmail.com" className="contact-item" target="_blank" rel="noopener noreferrer">
+          <a href="mailto:hamzas4011@hotmail.com" className="contact-item email" target="_blank" rel="noopener noreferrer">
             <FaEnvelope className="contact-icon" />
           </a>
 
-          <a href="https://www.linkedin.com/in/hamza-simsek-033a14108/" className="contact-item" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.linkedin.com/in/hamza-simsek-033a14108/" className="contact-item linkedin" target="_blank" rel="noopener noreferrer">
             <FaLinkedin className="contact-icon" />
           </a>
 
-          <a href="https://github.com/hamzas4011/" className="contact-item" target="_blank" rel="noopener noreferrer">
+          <a href="https://github.com/hamzas4011/" className="contact-item github" target="_blank" rel="noopener noreferrer">
             <FaGithub className="contact-icon" />
           </a>
         </div>
